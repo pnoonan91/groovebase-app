a01b4da1ad271894c466d4c4c8ce9de4d01c17c1
Patrick Noonan <pnoonan91@gmail.com>
2018-01-02 12:35:20 -0500

    basically done

--- app/components/userShows.jsx
+++ app/components/userShows.jsx
@@ -8,11 +8,13 @@ import {getSearch} from '../reducers/setlistSearch'
 import store from '../store.jsx'
 import { setUserSetlists } from '../reducers/userSetlists.js'
 import { setlistSearchResultsPage } from '../reducers/setlistSearch'
+import { setUserFavorites } from '../reducers/userFavorites.js'
 
 class UserShows extends Component {
   constructor(props){
     super(props)
     this.playlistSearch = this.playlistSearch.bind(this)
+    this.addFavorite = this.addFavorite.bind(this)
   }
 
   componentDidMount(){
@@ -22,6 +24,12 @@ class UserShows extends Component {
     .then(res => res.data)
     .then(setlists => {
       store.dispatch(setUserSetlists(setlists))
+    })
+
+    axios.get(`/api/shows/favorite/${currentUser.id}`)
+    .then(res => res.data)
+    .then(favorites => {
+      store.dispatch(setUserFavorites(favorites))
     })
   }
 
@@ -54,12 +62,41 @@ class UserShows extends Component {
     this.props.searchSetlist(search)
   }
 
+  addFavorite(element){
+    const {currentUser} = this.props
+    const imgSrc = element.target.id
+    axios.put(`/api/shows/favorite/${element.target.id}`)
+    .then(() => {
+      axios.get(`/api/shows/favorite/${currentUser.id}`)
+      .then(res => res.data)
+      .then(favorites => {
+        store.dispatch(setUserFavorites(favorites))
+      })
+      .then(() => {
+        let fav = document.getElementById(`${imgSrc}`).src.slice(-16)
+        if(fav === 'not-favorite.png'){
+          document.getElementById(`${imgSrc}`).src = "/favorite-icons/favorite.png"
+        } else {
+          document.getElementById(`${imgSrc}`).src = "/favorite-icons/not-favorite.png"
+        }
+
+      })
+      .then(() => {
+        axios.get(`/api/shows/${currentUser.id}`)
+        .then(res => res.data)
+        .then(setlists => {
+          store.dispatch(setUserSetlists(setlists))
+        })
+      })
+    })
+  }
+
   render() {
     const { currentUser, userSetlists } = this.props
     return (
       <div className="padding-container user-page">
         <div id="user-header">
-          <h1 className="header-text purple-text setlist-listing-header">{currentUser.firstName && currentUser.firstName}'s Setlist Archive</h1>
+          <h1 className="header-text purple-text">{currentUser.firstName && currentUser.firstName}'s Setlist Archive</h1>
           <div id="playlist-search">
             <h3 className="purple-text playlist-search-btn no-margin" onClick={this.toggleView}>+Search for a Setlist</h3>
           </div>
@@ -81,18 +118,43 @@ class UserShows extends Component {
         </div>
 
         <div id="user-setlist-listing">
-          {userSetlists.length && userSetlists.map(setlist =>
-            <div className="setlist-item">
-              <Link to={`/setlist/${setlist.setlistId}`}>
-              <div className="setlist-thumbnail">{setlist.artistName}</div>
-              </Link>
-              <div className="setlist-caption">
-                <span className="caption-header">{setlist.venueName}</span>
-                <p className="caption-location">{`${setlist.city}, ${setlist.stateCode}`}</p>
-                <p className="event-date">{setlist.eventDate}</p>
-              </div>
-            </div>
-          )}
+          <table className="table-results">
+            <tr className="show-page-table-header">
+              <th className="th-showpage">Artist</th>
+              <th className="th-showpage">Date</th>
+              <th className="th-showpage">Venue</th>
+              <th>Favorite</th>
+            </tr>
+            {userSetlists.length && userSetlists.sort(function(a, b) {
+              var nameA = a.artistName.toUpperCase()
+              var nameB = b.artistName.toUpperCase()
+              if(nameA < nameB){
+                return -1
+              }
+              if(nameA > nameB){
+                return 1
+              }
+              return 0
+            }).map(setlist => (
+              <tr className="table-listing" key={setlist.id}>
+                <td className="table-listing-item user-page-listing">
+                  <Link to={`/setlist/${setlist.setlistId}`}>
+                    <h3 className="purple-text no-margin underline-hover">{setlist.artistName}</h3>
+                  </Link>
+                </td>
+                <td className="table-listing-item user-page-listing">
+                  <p className="no-margin">{setlist.eventDate}</p>
+                </td>
+                <td className="table-listing-item user-page-listing">
+                  <p className="no-margin">{setlist.venueName}</p>
+                  <p className="no-margin">{`${setlist.city}, ${setlist.stateCode}`}</p>
+                </td>
+                <td className="table-listing" key={setlist.id}>
+                  <img src={setlist.isFavorite} className="user-page-favorite-icon" id={setlist.id} onClick={this.addFavorite}/>
+              </td>
+              </tr>
+            ))}
+          </table>
         </div>
       </div>
     )
@@ -101,10 +163,11 @@ class UserShows extends Component {
 
 /* -----------------    CONTAINER     ------------------ */
 
-const mapState = ({ currentUser, userSetlists }, ownProps) => {
+const mapState = ({ currentUser, userSetlists, userFavorites }, ownProps) => {
   return {
     currentUser,
-    userSetlists
+    userSetlists,
+    userFavorites
   }
 }
 
